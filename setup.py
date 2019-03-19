68af6fa20e0a1c0003bc70c5e83acb455f2514a8
Todd Leonhardt <todd.leonhardt@gmail.com>
2019-03-19 02:19:28 -0400

    Bump version to 0.1.3 and cleanup various things

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 from setuptools import setup
-try:
-    import multiprocessing
-except ImportError:
-    pass
+from httpie_aws_authv4 import __version__
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except ImportError:
-    long_description = open('README.md').read()
+long_description = open('README.md').read()
 
 setup(
     name='httpie-aws-authv4',
     description='AWS auth v4 plugin for HTTPie.',
-    version='0.1.2',
+    version=__version__,
     author='Aidan Rowe',
     author_email='aidanrowe@gmail.com',
     license='BSD',
@@ -22,26 +15,28 @@ setup(
     py_modules=['httpie_aws_authv4'],
     zip_safe=False,
     long_description=long_description,
+    long_description_content_type='text/markdown',
     entry_points={
         'httpie.plugins.auth.v1': [
             'httpie_aws_authv4 = httpie_aws_authv4:AWSv4AuthPlugin'
         ]
     },
     install_requires=[
-        'httpie>=0.9.7',
-        'requests-aws>=0.1.8',
-        'aws_requests_auth>=0.3.0',
-        'boto3>=1.4.0'
+        'httpie>=1.0.0',
+        'aws_requests_auth>=0.4.0',
+        'boto3>=1.9.0',
+        'urllib3'
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Environment :: Plugins',
         'License :: OSI Approved :: BSD License',
         'Topic :: Internet :: WWW/HTTP',
