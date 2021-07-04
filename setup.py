b908c345c59cb4f5499c7b840189e84b7f4e91cf
Aidan Rowe <aidanrowe@gmail.com>
2021-07-04 11:46:11 +0000

    fix module classifiers

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ setup(
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
         "Environment :: Plugins",
         "License :: OSI Approved :: BSD License",
         "Topic :: Internet :: WWW/HTTP",
