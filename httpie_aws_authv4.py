bfb6891fbf850886a3bfc675f770a1e52aaa6c57
Carl Fürstenberg <azatoth@gmail.com>
2022-04-15 02:28:00 +0200

    Add extended key=value parsing of the -a flag (#11)

--- httpie_aws_authv4.py
+++ httpie_aws_authv4.py
@@ -5,10 +5,9 @@ AWS-v4 auth plugin for HTTPie.
 """
 import re
 
-from httpie.plugins import AuthPlugin
-
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 from boto3 import session
+from httpie.plugins import AuthPlugin
 from urllib3.util import parse_url
 
 __version__ = "0.2.1"
@@ -17,8 +16,18 @@ __licence__ = "MIT"
 
 
 class AWSAuth(object):
-    def __init__(self, access_key=None, secret_key=None, domain=None, profile=None):
+    def __init__(
+        self,
+        access_key=None,
+        secret_key=None,
+        domain=None,
+        profile=None,
+        region=None,
+        service=None,
+    ):
         self.domain = domain
+        self.aws_region: str = region
+        self.aws_service: str = service
 
         if access_key and secret_key:
             self.aws_access_key = access_key
@@ -34,7 +43,7 @@ class AWSAuth(object):
 
     def __eq__(self, other):
         if not isinstance(other, AWSAuth):
-            raise NotImplemented
+            raise NotImplementedError
 
         return all(
             [
@@ -42,6 +51,8 @@ class AWSAuth(object):
                 self.aws_secret_access_key == other.aws_secret_access_key,
                 self.aws_token == other.aws_token,
                 self.domain == other.domain,
+                self.aws_region == other.aws_region,
+                self.aws_service == other.aws_service,
             ]
         )
 
@@ -52,7 +63,10 @@ class AWSAuth(object):
             if not host:
                 _, _, host, _, _, _, _ = parse_url(r.url)
                 r.headers["Host"] = host
-            if self.domain is not None:
+
+            if self.aws_region is not None and self.aws_service is not None:
+                aws_params = dict(region=self.aws_region, service=self.aws_service)
+            elif self.domain is not None:
                 aws_params = self._parse_url(self.domain)
             else:
                 aws_params = self._parse_url(host)
@@ -99,39 +113,73 @@ class AWSv4AuthPlugin(AuthPlugin):
     auth_parse = False
     prompt_password = False
 
+    alias_params = {
+        "ak": "access_key",
+        "sk": "secret_key",
+        "p": "profile",
+        "d": "domain",
+        "s": "service",
+        "r": "region",
+    }
+    """map of alias -> full param name"""
+
     def get_auth(self, username=None, password=None):
-        # To remain consistant with AWS tools, boto3 credential store is used
-        #   to retrieve the users API keys.  This means environment variables
-        #   take precedent over ~/.aws/credentials and IAM roles.  credentials
-        #   can be provided on the CLI using the -a flag
-        #       eg, -a <access_key>:<secret_access_key>
+        """
+        To remain consistant with AWS tools, boto3 credential store is used
+        to retrieve the users API keys.  This means environment variables
+        take precedent over ~/.aws/credentials and IAM roles.  credentials
+        can be provided on the CLI using the -a flag eg,
+            -a <access_key>:<secret_access_key>
 
-        # An attempt is made to try and determine the region, endpoint and
-        #   service from a given request URL.  If you are using a custom domain
-        #   this is no doubt going fail and the user will need to input the neccessary
-        #   information using the `-a` flag.
+        You can also provide aditional parameters using following syntax:
+            -a access_key=<access_key>,secret_key=<secret_key>,
+            profile=<profile>,domain=<domain>,service=<service>,region=<region>
 
-        # The format for the `-a` flag is:
-        #   "service:region:endpoint"
+        or short equvivalent:
+            -a ak=<access_key>,sk=<secret_key>,p=<profile>,d=<domain>,
+            s=<service>,r=<region>
 
-        access_key = None
-        secret_key = None
-        domain = None
-        profile = None
+        For example:
+            -a s=executeapi,r=eu-west-1
+
+        An attempt is made to try and determine the region, endpoint and
+        service from a given request URL if you are not supplying this data as above.
+        If you are using a custom domain this will fail and service plus region need
+        to be supplied in the -a flag.
+        """
+
+        params: dict = {}
 
         if self.raw_auth is not None:
-            parts = self.raw_auth.split(":")
-            if len(parts) >= 2:
-                if parts[0] == "profile":
-                    profile = parts[1]
-                else:
-                    access_key = parts[0]
-                    secret_key = parts[1]
-                if len(parts) == 3:
-                    domain = parts[2]
-            elif len(parts) == 1:
-                domain = parts[0]
 
-        return AWSAuth(
-            access_key=access_key, secret_key=secret_key, domain=domain, profile=profile
-        )
+            try:
+                if '=' in self.raw_auth:
+
+                    params = dict(
+                        x.split('=', 2) for x in self.raw_auth.split(',')
+                    )
+
+                    for alias, full in self.alias_params.items():
+                        if alias in params:
+                            params[full] = params[alias]
+                            del params[alias]
+
+                else:
+
+                    parts = self.raw_auth.split(":")
+                    if len(parts) >= 2:
+                        if parts[0] == "profile":
+                            params["profile"] = parts[1]
+                        else:
+                            params["access_key"] = parts[0]
+                            params["secret_key"] = parts[1]
+                        if len(parts) == 3:
+                            params["domain"] = parts[2]
+                    elif len(parts) == 1:
+                        params["domain"] = parts[0]
+
+            except ValueError:
+                print("ERROR: Could not parse auth string.")
+                raise
+
+        return AWSAuth(**params)
--- tests/test_httpie_aws_authv4.py
+++ tests/test_httpie_aws_authv4.py
@@ -1,7 +1,7 @@
-import pytest
-import mock
+from unittest.mock import Mock
 
 import httpie_aws_authv4
+import pytest
 
 
 @pytest.mark.parametrize(
@@ -42,12 +42,12 @@ def test_auth_parsing_static_keys(input, expected):
     ],
 )
 def test_auth_parsing_profile(mocker, input, expected_profile, expected_extra):
-    m = mock.Mock()
+    m = Mock()
     m.access_key = "mock_access_key"
     m.secret_key = "mock_secret_key"
     m.token = "mock_token"
 
-    class MockSession(mock.Mock):
+    class MockSession(Mock):
         def get_credentials(self):
             return m
 
@@ -57,11 +57,56 @@ def test_auth_parsing_profile(mocker, input, expected_profile, expected_extra):
     auth.raw_auth = input
     res = auth.get_auth()
 
-    print(res.aws_access_key)
-
     assert res.aws_access_key == "mock_access_key"
     assert res.aws_secret_access_key == "mock_secret_key"
     assert res.aws_token == "mock_token"
 
     for attribute, value in expected_extra.items():
+        assert getattr(res, attribute) == value
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        ("", {}),
+        ("profile=XXX", {}),
+        ("p=XXX", {}),
+        ("service=XXX", {"aws_service": "XXX"}),
+        ("s=XXX", {"aws_service": "XXX"}),
+        ("region=XXX", {"aws_region": "XXX"}),
+        ("r=XXX", {"aws_region": "XXX"}),
+        ("domain=XXX", {"domain": "XXX"}),
+        ("d=XXX", {"domain": "XXX"}),
+        ('access_key=XXX', {"aws_access_key": "mock_access_key"}),
+        (
+            'access_key=XXX,secret_key=YYY',
+            {"aws_access_key": "XXX", "aws_secret_access_key": "YYY"},
+        ),
+        (
+            'ak=XXX,sk=YYY',
+            {"aws_access_key": "XXX", "aws_secret_access_key": "YYY"},
+        ),
+        ("service=XXX,region=YYY", {"aws_service": "XXX", "aws_region": "YYY"}),
+        ("s=XXX,r=YYY", {"aws_service": "XXX", "aws_region": "YYY"}),
+    ],
+)
+def test_auth_parsing_comma_separated(mocker, input, expected):
+    m = Mock()
+    m.access_key = "mock_access_key"
+    m.secret_key = "mock_secret_key"
+    m.token = "mock_token"
+
+    class MockSession(Mock):
+        def get_credentials(self):
+            return m
+
+    mocker.patch("httpie_aws_authv4.session.Session", MockSession)
+
+    auth = httpie_aws_authv4.AWSv4AuthPlugin()
+    auth.raw_auth = input
+    res = auth.get_auth()
+
+    print(res)
+
+    for attribute, value in expected.items():
         assert getattr(res, attribute) == value
